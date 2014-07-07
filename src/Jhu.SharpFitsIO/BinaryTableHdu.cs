98e35b0f7fe0be80ee2fde9dae6ce29f39a09297
Laszlo Dobos <dobos@complex.elte.hu>
2014-07-07 15:25:11 +0200

    Comments added. Minimal refactoring.

--- src/Jhu.SharpFitsIO/BinaryTableHdu.cs
+++ src/Jhu.SharpFitsIO/BinaryTableHdu.cs
@@ -11,18 +11,28 @@ using System.Linq.Expressions;
 
 namespace Jhu.SharpFitsIO
 {
+    /// <summary>
+    /// Implements function to read and write FITS binary tables in a streaming fashion.
+    /// </summary>
+    /// <remarks>
+    /// The class does not provide storage for table data, it only reads and writes from disk.
+    /// </remarks>
     public class BinaryTableHdu : SimpleHdu, ICloneable
     {
-        // TODO: cache compiled lambdas to prevent leaks
-
         private delegate int BinaryReaderDelegate(BitConverterBase converter, FitsTableColumn column, byte[] buffer, int startIndex, out object value);
         private delegate int BinaryWriterDelegate(BitConverterBase converter, FitsTableColumn column, byte[] buffer, int startIndex, object value);
 
         #region Private member variables
 
+        /// <summary>
+        /// Holds binary reader delegates associated with each column
+        /// </summary>
         [NonSerialized]
         private BinaryReaderDelegate[] columnByteReaders;
 
+        /// <summary>
+        /// Holds binary writer delegates associates with each column
+        /// </summary>
         [NonSerialized]
         private BinaryWriterDelegate[] columnByteWriters;
 
@@ -50,6 +60,9 @@ namespace Jhu.SharpFitsIO
         #endregion
         #region Keyword accessor properties
 
+        /// <summary>
+        /// Gets the number of columns, a value based on the headers.
+        /// </summary>
         public int ColumnCount
         {
             get { return Cards[Constants.FitsKeywordTFields].GetInt32(); }
@@ -63,17 +76,26 @@ namespace Jhu.SharpFitsIO
             }
         }
 
+        /// <summary>
+        /// Gets or sets the number of rows in the table.
+        /// </summary>
         public int RowCount
         {
             get { return GetAxisLength(2); }
             set { SetAxisLength(2, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the offset of the heap storage area. Not used.
+        /// </summary>
         public int HeapOffset
         {
             get { return Cards[Constants.FitsKeywordTHeap].GetInt32(); }
         }
 
+        /// <summary>
+        /// Gets or sets the number of parameters. Not used.
+        /// </summary>
         public int ParameterCount
         {
             get { return Cards[Constants.FitsKeywordPCount].GetInt32(); }
@@ -87,6 +109,9 @@ namespace Jhu.SharpFitsIO
             }
         }
 
+        /// <summary>
+        /// Gets or sets the number of data groups. Not used.
+        /// </summary>
         public int GroupCount
         {
             get { return Cards[Constants.FitsKeywordGCount].GetInt32(); }
@@ -103,12 +128,23 @@ namespace Jhu.SharpFitsIO
         #endregion
         #region Constructors and initializers
 
+        /// <summary>
+        /// Creates and initializes a new binary table associated with a FITS file.
+        /// </summary>
+        /// <param name="fits"></param>
         internal BinaryTableHdu(FitsFile fits)
             : base(fits)
         {
             InitializeMembers();
         }
 
+        /// <summary>
+        /// Creates and initializes a new binary table based on a simple HDU.
+        /// </summary>
+        /// <param name="hdu"></param>
+        /// <remarks>
+        /// Used during automatic HDU recognition.
+        /// </remarks>
         internal BinaryTableHdu(SimpleHdu hdu)
             : base(hdu)
         {
@@ -117,12 +153,19 @@ namespace Jhu.SharpFitsIO
             DetectColumns();
         }
 
-        private BinaryTableHdu(BinaryTableHdu old)
+        /// <summary>
+        /// Creates a copy of an existing binary table without copying data.
+        /// </summary>
+        /// <param name="old"></param>
+        internal BinaryTableHdu(BinaryTableHdu old)
             : base(old)
         {
             CopyMembers(old);
         }
 
+        /// <summary>
+        /// Initializes member variables
+        /// </summary>
         private void InitializeMembers()
         {
             this.columnByteReaders = null;
@@ -130,6 +173,10 @@ namespace Jhu.SharpFitsIO
             this.columns = new List<FitsTableColumn>();
         }
 
+        /// <summary>
+        /// Copies member variables
+        /// </summary>
+        /// <param name="old"></param>
         private void CopyMembers(BinaryTableHdu old)
         {
             this.columnByteReaders = null;
@@ -141,6 +188,10 @@ namespace Jhu.SharpFitsIO
             }
         }
 
+        /// <summary>
+        /// Creates a deep clone of the object.
+        /// </summary>
+        /// <returns></returns>
         public override object Clone()
         {
             return new BinaryTableHdu(this);
@@ -149,11 +200,18 @@ namespace Jhu.SharpFitsIO
         #endregion
         #region Static create functions
 
-        public static BinaryTableHdu Create(FitsFile fits, bool initialize)
+        /// <summary>
+        /// Creates a new binary table associated with a FITS file. Optionally
+        /// initializes headers.
+        /// </summary>
+        /// <param name="fits"></param>
+        /// <param name="initializeHeaders"></param>
+        /// <returns></returns>
+        public static BinaryTableHdu Create(FitsFile fits, bool initializeHeaders)
         {
             var hdu = new BinaryTableHdu(fits);
 
-            if (initialize)
+            if (initializeHeaders)
             {
                 hdu.InitializeCards(false, false);
             }
@@ -164,6 +222,11 @@ namespace Jhu.SharpFitsIO
         #endregion
         #region Card functions
 
+        /// <summary>
+        /// Initializes header cards to their default values.
+        /// </summary>
+        /// <param name="primary"></param>
+        /// <param name="hasExtension"></param>
         protected override void InitializeCards(bool primary, bool hasExtension)
         {
             base.InitializeCards(primary, hasExtension);
@@ -182,6 +245,10 @@ namespace Jhu.SharpFitsIO
         #endregion
         #region Column functions
 
+        /// <summary>
+        /// Returns the length of a single row in bytes.
+        /// </summary>
+        /// <returns></returns>
         public override int GetStrideLength()
         {
             int res = 0;
@@ -208,7 +275,7 @@ namespace Jhu.SharpFitsIO
                 columns.Add(FitsTableColumn.CreateFromCards(this, i + 1));
             }
 
-            InitializeColumnByteReaders();
+            InitializeColumnBinaryReaders();
 
             // Verify size
             if (GetAxisLength(1) != GetStrideLength())
@@ -289,6 +356,13 @@ namespace Jhu.SharpFitsIO
             CreateColumns(columns);
         }
 
+        /// <summary>
+        /// Returns the size of a field of a struct in bytes, based on marshaling attributes.
+        /// </summary>
+        /// <param name="field"></param>
+        /// <param name="type"></param>
+        /// <param name="repeat"></param>
+        /// <returns></returns>
         private bool GetFieldArraySize(FieldInfo field, out Type type, out int repeat)
         {
             var attr = field.GetCustomAttributes(typeof(MarshalAsAttribute), false);
@@ -316,7 +390,10 @@ namespace Jhu.SharpFitsIO
             }
         }
 
-        private void InitializeColumnByteReaders()
+        /// <summary>
+        /// Initializes binary reader delegates for each column.
+        /// </summary>
+        private void InitializeColumnBinaryReaders()
         {
             columnByteReaders = new BinaryReaderDelegate[Columns.Count];
 
@@ -326,7 +403,10 @@ namespace Jhu.SharpFitsIO
             }
         }
 
-        private void InitializeColumnByteWriters()
+        /// <summary>
+        /// Initializes binary writer delegates for each column.
+        /// </summary>
+        private void InitializeColumnBinaryWriters()
         {
             if (columnByteWriters == null)
             {
@@ -341,6 +421,11 @@ namespace Jhu.SharpFitsIO
 
         #endregion
 
+        /// <summary>
+        /// Reads the next row from the binary table into an array of objects.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
         public bool ReadNextRow(object[] values)
         {
             if (HasMoreStrides)
@@ -368,6 +453,10 @@ namespace Jhu.SharpFitsIO
             }
         }
 
+        /// <summary>
+        /// Writes the next row into a binary table. Parameter types must match columns.
+        /// </summary>
+        /// <param name="values"></param>
         public void WriteNextRow(params object[] values)
         {
             if (StrideBuffer == null)
@@ -375,7 +464,7 @@ namespace Jhu.SharpFitsIO
                 CreateStrideBuffer();
             }
 
-            InitializeColumnByteWriters();
+            InitializeColumnBinaryWriters();
 
             int startIndex = 0;
             for (int i = 0; i < Columns.Count; i++)
@@ -714,6 +803,12 @@ namespace Jhu.SharpFitsIO
         #endregion
         #region Write delegate generator functions
 
+        /// <summary>
+        /// Returns a delegate to write a boxed, strongly typed variable into
+        /// binary format.
+        /// </summary>
+        /// <param name="column"></param>
+        /// <returns></returns>
         private BinaryWriterDelegate CreateBinaryWriterDelegate(FitsTableColumn column)
         {
             // Complex types firts, then scalar and arrays
